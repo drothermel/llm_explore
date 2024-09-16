99dc30198133820083a26e7236a2fcc4153e766a
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-09-15 23:51:13 -0400

    fix bug I introduced in editting model.py

--- open_lm/src/model.py
+++ open_lm/src/model.py
@@ -223,7 +223,7 @@ class Block(nn.Module):
         self.head_dim = args.dim // args.n_heads
         self.attention = CustomAttn(layer_id, args)
         self._ffn_type = args.ffn_type
-        elif args.ffn_type == "swiglu_torch":
+        if args.ffn_type == "swiglu_torch":
             # this follows llama / lit llama -- go to multiple of 256
             self.hidden_dim = 256 * ((int(2 * 4 * args.dim / 3) + 256 - 1) // 256)
             self.feed_forward = SwiGLUTorch(args.dim, self.hidden_dim, args.dim, bias=False)
