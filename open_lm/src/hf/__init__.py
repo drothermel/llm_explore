97d7b9e3e3af5a0a5ec28985554538988b7f641d
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-09-16 00:01:24 -0400

    updated the hf init file with strange lint fix

--- open_lm/src/hf/__init__.py
+++ open_lm/src/hf/__init__.py
@@ -1,3 +1,3 @@
-from .configuration_openlm import OpenLMConfig
-from .modeling_openlm import OpenLMForCausalLM
-from .tokenization_openlm import OpenLMTokenizerFast
+from .configuration_openlm import OpenLMConfig as OpenLMConfig
+from .modeling_openlm import OpenLMForCausalLM as OpenLMForCausalLM
+from .tokenization_openlm import OpenLMTokenizerFast as OpenLMTokenizerFast
